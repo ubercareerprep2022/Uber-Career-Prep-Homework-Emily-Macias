1d7dc5b64ca2758554c6469aca4e3a8d30b2dec4
EmilyMacias <99148529+EmilyMacias@users.noreply.github.com>
2022-06-24 10:49:54 -0700

    Add files via upload

---
+++ phoneTree.h
@@ -0,0 +1,14 @@
+#ifndef PHONETREE_H
+#define PHONETREE_H
+
+#endif // PHONETREE_H
+#include <string>  //Why do we need to include <string> ?
+
+struct node{
+    std::string name;
+    long phoneNumber;
+    node* left;
+    node* right;
+};
+
+node* root = nullptr;
